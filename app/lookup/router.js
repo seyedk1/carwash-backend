f9231ea0a0f0c1341286185b3796d40bcb30d1b8
seyed keyvan nobakht <seyed.keyvan.nobakht@hotmail.com>
2021-09-27 20:11:23 +0330

    some edit in lookup for authorize route

--- app/lookup/router.js
+++ app/lookup/router.js
@@ -1,29 +1,31 @@
+require('module-alias/register')
+
 const { Router } = require("express");
 
 const lookupController = require("./controller");
-// const { authenticated } = require("@root/core/middleware/auth")
+const { authenticated } = require("@root/core/middleware/auth")
 
 
 const router = new Router();
 
 //  @desc   Create Lookup Handle
 //  @route  POST /api/lookup/create-lookup
-router.post("/create-lookup", lookupController.createLookup);
+router.post("/create-lookup", authenticated, lookupController.createLookup);
 
 //  @desc   Get All Data From Lookup
 //  @route  GET /api/lookup/get-all-lookup
-router.get("/get-all-lookups", lookupController.getAllLookups);
+router.get("/get-all-lookups", authenticated, lookupController.getAllLookups);
 
 //  @desc   Get Single Data From Lookup
-//  @route  GET /api/lookup/get-single-lookup
-router.get("/get-single-lookup/:id", lookupController.getSingleLookup);
+//  @route  GET /api/lookup/get-single-lookup/:id
+router.get("/get-single-lookup/:id", authenticated, lookupController.getSingleLookup);
 
 //  @desc   Update Data From Lookup
-//  @route  PUT /api/lookup/edit-lookup
-router.put("/edit-lookup/:id", lookupController.editLookup);
+//  @route  PUT /api/lookup/edit-lookup/:id
+router.put("/edit-lookup/:id", authenticated, lookupController.editLookup);
 
 //  @desc   Delete Data From Lookup
-//  @route  DELETE /api/lookup/delete-lookup
-router.delete("/delete-lookup/:id", lookupController.deleteLookup);
+//  @route  DELETE /api/lookup/delete-lookup/:id
+router.delete("/delete-lookup/:id", authenticated, lookupController.deleteLookup);
 
 module.exports = router;
